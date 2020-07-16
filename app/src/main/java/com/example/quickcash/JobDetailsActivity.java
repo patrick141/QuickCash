35ddaa0dc08839d0c1c532bd3281e0fbdf823b97
Patrick Amaro Rivera <patamaro1@fb.com>
2020-07-16 16:46:13 -0400

    created jobs detail activity layout

--- app/src/main/java/com/example/quickcash/JobDetailsActivity.java
+++ app/src/main/java/com/example/quickcash/JobDetailsActivity.java
@@ -1,14 +1,21 @@
 package com.example.quickcash;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quickcash.databinding.ActivityJobDetailsBinding;
 
 public class JobDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_details);
+        ActivityJobDetailsBinding binding = ActivityJobDetailsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 }
