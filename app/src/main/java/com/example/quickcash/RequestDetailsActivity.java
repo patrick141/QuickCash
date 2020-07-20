1537a25adcc99ae11edaa9ec2d930dd4351287b8
Patrick Amaro Rivera <patamaro1@fb.com>
2020-07-20 14:14:01 -0400

    User can assign a user to a job

--- app/src/main/java/com/example/quickcash/RequestDetailsActivity.java
+++ app/src/main/java/com/example/quickcash/RequestDetailsActivity.java
@@ -1,6 +1,7 @@
 package com.example.quickcash;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -10,8 +11,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.example.quickcash.databinding.ActivityRequestDetailsBinding;
+import com.example.quickcash.models.Job;
 import com.example.quickcash.models.Request;
+import com.parse.DeleteCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.SaveCallback;
 
 import org.parceler.Parcels;
 
@@ -52,5 +57,39 @@ public class RequestDetailsActivity extends AppCompatActivity {
 
         tvRequestor.setText(request.getUser().getUsername());
         tvRequestorComment.setText(request.getComment());
+
+        btnAcceptUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Job job = (Job) request.getJob();
+                job.setAssignedUser(request.getUser());
+                job.setIsTaken(true);
+                job.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e!= null){
+                            Log.e(TAG, "Error giving " + request.getUser().getUsername() + " this job.", e);
+                        }
+                        Log.i(TAG, request.getUser().getUsername() + " has this job!");
+                        finish();
+                    }
+                });
+            }
+        });
+        btnDenyUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                request.deleteInBackground(new DeleteCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e != null){
+                            Log.e(TAG, "Error deleting this request", e);
+                        }
+                        Log.i(TAG, "Going to delete this request");
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
