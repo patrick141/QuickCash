231ba1aa49ad4f7a9de294e7d6d8577a2c345d7b
Patrick Amaro Rivera <patamaro1@fb.com>
2020-07-31 18:15:26 -0400

    Enable ParseLiveQuery on QuickCash

--- app/src/main/java/com/example/quickcash/ParseApplication.java
+++ app/src/main/java/com/example/quickcash/ParseApplication.java
@@ -7,6 +7,10 @@ import com.example.quickcash.models.Notification;
 import com.example.quickcash.models.Request;
 import com.parse.Parse;
 import com.parse.ParseObject;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+
 /**
  * ParseApplication
  *
@@ -26,11 +30,22 @@ public class ParseApplication extends Application {
         ParseObject.registerSubclass(Request.class);
         ParseObject.registerSubclass(Notification.class);
 
+        /**
+         * This is to monitor our Parse network traffic
+         */
+        // Use for monitoring Parse network traffic
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
         /**This is calling our Heroku Parse DB based off of
          * our parameters.
          */
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("patrick-quickcash")
+                .clientBuilder(builder)
                 .clientKey("CodepathMoveFaster")
                 .server("https://patrick-quickcash.herokuapp.com/parse").build());
     }
