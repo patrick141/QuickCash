2de92d8078ca40f5e532b46b65f17e9183e290b3
Patrick Amaro Rivera <patamaro1@fb.com>
2020-08-06 00:14:26 -0400

    Added functions to Friends class

--- app/src/main/java/com/example/quickcash/models/Friends.java
+++ app/src/main/java/com/example/quickcash/models/Friends.java
@@ -1,4 +1,43 @@
 package com.example.quickcash.models;
 
-public class Friends {
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Friends
+ *
+ * The Friends class holds our friends list for each user.
+ */
+@ParseClassName("Friends")
+public class Friends extends ParseObject {
+    public static final String KEY_USER = "user";
+    public static final String KEY_FRIEND_LIST = "friendsList";
+
+    public ParseUser getUser(){
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+    public ArrayList<ParseUser> getFriendsList(){
+        return (ArrayList<ParseUser>) get(KEY_FRIEND_LIST);
+    }
+
+    public void setFriendList(ArrayList<ParseUser> friendList){
+        put(KEY_FRIEND_LIST, friendList);
+    }
+
+    public void addFriend(ParseUser user){
+        add(KEY_FRIEND_LIST, user);
+    }
+
+    public void removeFriend(ParseUser user){
+        removeAll(KEY_FRIEND_LIST, Arrays.asList(user));
+    }
 }
