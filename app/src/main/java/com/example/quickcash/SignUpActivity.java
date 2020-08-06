ade4d624f1a82c24ff6b87ae3cfb2b55c3abeaf6
Patrick Amaro Rivera <patamaro1@fb.com>
2020-08-06 00:01:32 -0400

    User must sign up with email and phone in SignUpActivity.

--- app/src/main/java/com/example/quickcash/SignUpActivity.java
+++ app/src/main/java/com/example/quickcash/SignUpActivity.java
@@ -11,9 +11,13 @@ import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.quickcash.databinding.ActivitySignUpBinding;
+import com.example.quickcash.models.User;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * SignUpActivity
@@ -27,6 +31,8 @@ public class SignUpActivity extends LoginActivity {
     public static final String TAG = "SignUpActivity";
     private EditText etNewUsername;
     private EditText etNewPassword;
+    private EditText etEmail;
+    private EditText etPhone;
     private Button btnSignUpNew;
     private Toolbar toolbar;
 
@@ -42,6 +48,8 @@ public class SignUpActivity extends LoginActivity {
 
         etNewUsername = binding.SAUsername;
         etNewPassword = binding.SAPassword;
+        etEmail = binding.SAEmail;
+        etPhone = binding.SAPhone;
         btnSignUpNew = binding.SABtnSignUp;
         toolbar = binding.toolbarSu;
         toolbar.setTitle(getString(R.string.sign_up_text));
@@ -55,12 +63,21 @@ public class SignUpActivity extends LoginActivity {
             public void onClick(View view) {
                 final String username = etNewUsername.getText().toString();
                 final String password = etNewPassword.getText().toString();
+                final String email = etEmail.getText().toString();
+                final String phone = etPhone.getText().toString();
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty() || phone.isEmpty()){
+                    Toast.makeText(SignUpActivity.this, getString(R.string.sa_enterAll), Toast.LENGTH_SHORT).show();
+                }
+                if(!isValidEmail(email) || !isValidPhone(phone) ){
+                    Toast.makeText(SignUpActivity.this, getString(R.string.sa_get_valid), Toast.LENGTH_SHORT).show();
+                }
                 Toast.makeText(SignUpActivity.this, "Signing up " + username, Toast.LENGTH_SHORT).show();
-
                 //We are creating a new account based off the user's details.
                 ParseUser user = new ParseUser();
                 user.setUsername(username);
                 user.setPassword(password);
+                user.setEmail(email);
+                user.put(User.KEY_USER_PHONE,phone);
                 user.signUpInBackground(new SignUpCallback() {
                     @Override
                     public void done(ParseException e) {
@@ -77,6 +94,34 @@ public class SignUpActivity extends LoginActivity {
                 });
             }
         });
+    }
+
+    /**
+     * This methods ensures that the email that is entered is valid. It uses a pattern and matcher
+     * to determine if it fits the typical email style.
+     * @param email
+     * @return
+     */
+    private boolean isValidEmail(String email){
+        String ePattern = getString(R.string.sa_email_valid);
+        Pattern p = Pattern.compile(ePattern);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    /**
+     * This method ensures that the phone number entered is valid. Uses Pattern and Matcher to deter
+     * mine if the phone number meets criteria.
+     * @param phone
+     * @return
+     */
+    private boolean isValidPhone(String phone){
+        String num1 = phone.replace("(","");
+        String num2 = num1.replace(")","");
+        String number = num2.replace("-","");
+        Pattern p = Pattern.compile(getString(R.string.sa_phone_valid));
+        Matcher m = p.matcher(number);
+        return (m.find() && m.group().equals(number));
     }
 
     /**
